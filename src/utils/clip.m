476e1c2a9236a558e9d8039ef1a3b6925f36a668
Jared Miller <jared.fr.miller@gmail.com>
2026-07-14 13:16:11 +0200

    CONS=true works with periodic orbit with prox operators
    the failure of the regulator equations was due to a bug in the prox (bw) implementation. This will need to be fixed everywhere op_sim is defined.
    
    CONS=true is still non-convergent. There may be an issue in the current projection codes, getting the wrong weights in the `clip' functions.

--- src/utils/clip.m
+++ src/utils/clip.m
@@ -1,9 +1,15 @@
-function c =  clip(x, lower, upper)
+function xclip =  clip(x, lower, upper)
+
+    c = size(lower, 1);
+    x = reshape(x, c, []);
     sl = x <= lower;
     su = x >= upper;
 
-    c = x;
-    c(sl) = lower;
-    c(su) = upper;
-
+    xclip = x;
+    for i = 1:c
+        xclip(sl(i, :)) = lower(i);
+        xclip(su(i, :)) = upper(i);
+    end
+    
+    xclip = reshape(xclip, [], 1);
 end
